09d6ce4aca7794b2548b5d7626748b2af72151b8
BuildTools <unconfigured@null.spigotmc.org>
2024-12-23 14:00:15 -0300

    fix file indentation

--- Plugin.cs
+++ Plugin.cs
@@ -10,7 +10,7 @@ namespace EggStatus;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
+
     private void Awake()
     {
         // Plugin startup logic
@@ -21,24 +21,25 @@ public class Plugin : BaseUnityPlugin
     }
 
     [HarmonyPostfix]
-	[HarmonyPatch(typeof(EggGrow), nameof(EggGrow.GetHoverText))]
-	public static string EggGetHoverText_Patch(string __result, EggGrow __instance)
-	{
-		if (__instance == null) return __result;
+    [HarmonyPatch(typeof(EggGrow), nameof(EggGrow.GetHoverText))]
+    public static string EggGetHoverText_Patch(string __result, EggGrow __instance)
+    {
+        if (__instance == null) return __result;
 
         int remainTime = GetRemainTimeToEggGrowUp(__instance);
 
         if (remainTime <= 0) return __result;
 
         return __result.Replace(")", $", Born in {remainTime}s)");
-	}
+    }
 
-    public static int GetRemainTimeToEggGrowUp(EggGrow __instance) {
+    public static int GetRemainTimeToEggGrowUp(EggGrow __instance)
+    {
         ZDO zdo = Traverse.Create(__instance).Field("m_nview").Method("GetZDO").GetValue<ZDO>();
         if (zdo == null) return 0;
 
         float num = zdo.GetFloat(ZDOVars.s_growStart);
 
-        return (int)(num + __instance.m_growTime) - (int) ZNet.instance.GetTimeSeconds();
+        return (int)(num + __instance.m_growTime) - (int)ZNet.instance.GetTimeSeconds();
     }
 }
